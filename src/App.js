3214e1baa512781f1f7233bb4c753979b4651c86
dania <daniadeyar@gmail.com>
2022-11-02 09:11:40 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import Header from "./Components/Header";
+import Countries from "./Components/Countries";
+import Filter from "./Components/Filter";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Filter />
+      <Countries />
+    </div>
+  );
+};
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,27 +6,9 @@ import Filter from "./Components/Filter";
 import "./index.css";
 import App from "./App";
 
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-const Home = () => {
-  return (
-    <div>
-      <Header />
-      <Filter />
-      <Countries />
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
